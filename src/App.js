4e7f3ad529bba1709b3f1455ed101dc079789c21
Byron Buckley <byronbuckley@live.com>
2016-10-29 12:50:55 -0500

    instruct - add: p2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import React, { Component } from 'react';
 import Instruct from './components/Instruct'
 import MarsList from './components/MarsList'
 import MarsGrid from './components/MarsGrid'
+import { default as Store } from './store';
+
+const mars = new Store();
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.addToStore = this.addToStore.bind(this);
+
+    // getInitialState
+    this.state = {
+      initialStore: {},
+      store: {},
+      lostList: {}
+    }
+  }
+
+  addToStore(martian) {
+    // update state cuz y'all need to see
+    const { name, x, y, isAlive, type } = martian;
+    console.log(name, x, y, isAlive, type, martian.toString(type));
+    /*if(mars.getAll().size < mars.add(martian)) {
+      const store = {...this.state.store};
+      store[martian.name] = {...martian};
+      this.setState({ store: store });
+    }*/
+
+    // persist in native format to do stuff later
+  }
+
   render() {
     return (
       <div className="App">
         <div>
-          <Instruct/>
+          <Instruct addToStore={this.addToStore} />
           <MarsList/>
         </div>
         <div>
--- src/components/Instruct.jsx
+++ src/components/Instruct.jsx
@@ -2,38 +2,42 @@ import React, { Component } from 'react';
 import { demoInstructions } from '../helpers';
 import { bounds } from '../config';
 import { instruct } from '../controller';
-import { Robot } from '../classes/martianRobot';
+import Robot from '../classes/martianRobot';
 
 class Instruct extends Component {
   constructor() {
     super();
+
     this.submitInstructions = this.submitInstructions.bind(this);
     this.validateInstruction = this.validateInstruction.bind(this);
   }
 
   submitInstructions(e) {
     e.preventDefault();
-    console.log(this.textInstructions.value);
 
     const inputArr = this.textInstructions.value.split("\n\n");
-    const beforeInstructions = [], afterInstructions = [];
+    // const beforeInstructions = [], afterInstructions = [];
 
-		inputArr.forEach(function(instruction, i) {
+		inputArr.forEach((instruction, i) => {
 			let currentInstructionSet = instruction.split("\n");
+
 			if (i === 0) {
 				var defaultsArr = currentInstructionSet[0].split(" ");
 				bounds.x = defaultsArr[0];
 				bounds.y = defaultsArr[1];
 				currentInstructionSet.shift(); // after we get the bounds delete its element from the instruction array.
 			}
-
 			let posArr = currentInstructionSet[0].trim().split(" ");
 
       // next linee should be conditional
-      let martian = new Robot('', posArr[0], posArr[1], posArr[2]);  // create a martian/robot with the line 1 of each instruction pair
-      beforeInstructions.push(martian);
-      afterInstructions.push(instruct(martian, currentInstructionSet[1]));
+      let martian = new Robot('', Number.parseInt(posArr[0], 10), Number.parseInt(posArr[1], 10), posArr[2]);  // create a martian/robot with the line 1 of each instruction pair
+
+      // beforeInstructions.push(martian);
+      this.props.addToStore(instruct(martian, currentInstructionSet[1]));
 		});
+
+    // const { name, x, y, isAlive, type, toString } = a;
+    // const aLite = {...a};
 
     // Initialize - show initial position on grid by setting state
   }
@@ -51,7 +55,7 @@ class Instruct extends Component {
   render() {
     return (
       <form
-        onSubmit={this.submitInstructions}>
+        onSubmit={(e) => this.submitInstructions(e)}>
         <textarea ref={(input) => {this.textInstructions = input}}
           onKeyUp={this.validateInstruction}
           name="" id="" cols="30" rows="10"
