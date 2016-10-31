eaa24aa415f1befc2212acf26db83e05280a74f7
Byron Buckley <byronbuckley@live.com>
2016-10-31 10:47:30 -0500

    lint: marsgrid

--- src/components/MarsGrid.jsx
+++ src/components/MarsGrid.jsx
@@ -19,18 +19,18 @@ class MarsGrid extends Component {
       .filter(key => store[key].type === 'Martian')
       .map(value => store[value]);
 
-    const {ScatterChart, Scatter, XAxis, YAxis, ZAxis, CartesianGrid, Tooltip, Legend} = Recharts;
+      const { ScatterChart, Scatter, XAxis, YAxis, ZAxis, CartesianGrid, Tooltip, Legend } = Recharts;
 
     return (
-    	<ScatterChart width={400} height={400} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
-      	<XAxis dataKey={'x'} name='x' />
-      	<YAxis dataKey={'y'} name='y' />
+      <ScatterChart width={400} height={400} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+        <XAxis dataKey={'x'} name='x' />
+        <YAxis dataKey={'y'} name='y' />
         {/* <ZAxis dataKey={'z'} range={[60, 400]} name='score' unit='km'/> */}
         <CartesianGrid />
-      	<Tooltip cursor={{strokeDasharray: '3 3'}} />
-        <Legend/>
-      	<Scatter name='Robots' data={robots} fill='gray' shape="triangle"/>
-        <Scatter name='Martians' data={martians} fill='red' shape="wye"/>
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Legend />
+        <Scatter name='Robots' data={robots} fill='gray' shape="triangle" />
+        <Scatter name='Martians' data={martians} fill='red' shape="wye" />
       </ScatterChart>
     );
   }
