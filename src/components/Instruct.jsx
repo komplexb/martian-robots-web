6c736a63333e639480008c3b4b0059499801cb7e
Byron Buckley <byronbuckley@live.com>
2016-11-13 17:18:30 -0500

    instruct: allow min 2 lines to support adding new instructions

--- src/components/Instruct.jsx
+++ src/components/Instruct.jsx
@@ -15,20 +15,20 @@ export default class Instruct extends Component {
 
     this.state = {txtInstructions: ''};
   }
-  
+
   /*
    * Set state with the instruction value so we can use it
-   * to update a couple UI elements. 
+   * to update a couple UI elements.
    * Since this is just 'component state', we don't need it to be persisted
    * and it doesn't need to be managed at app level.
    * @txtInstructions: set by #txtIntructions
-   * 
+   *
    */
   handleInstructionChange = (event) => {
     this.setState({txtInstructions: event.target.value});
   }
 
-  
+
   /**
    * Make it easier for users to get started by using predefined instructions
    */
@@ -36,10 +36,10 @@ export default class Instruct extends Component {
     e.preventDefault();
     this.setState({txtInstructions: demoInstructions});
   }
-  
-  
+
+
   /**
-   * Handler for submitting instructions and updating application 
+   * Handler for submitting instructions and updating application
    * state with data required to render MarsList, MarsGrid etc.
    * TODO: Add error text for failed instructions.
    */
@@ -57,8 +57,8 @@ export default class Instruct extends Component {
         if (bounds.isSet === false) {
           bounds.x = defaultsArr[0];
           bounds.y = defaultsArr[1];
+          currentInstructionSet.shift(); // after we get the bounds delete its element from the instruction array.
         }
-        currentInstructionSet.shift(); // after we get the bounds delete its element from the instruction array.
       }
 
       const [martianStr, instructionsStr] = currentInstructionSet;
@@ -72,19 +72,21 @@ export default class Instruct extends Component {
       return instruct(new Robot(chance.syllable({length: 5}), Number.parseInt(x, 10), Number.parseInt(y, 10), o), instructionsStr);
     });
     this.props.addToStore(afterInstructions);
+    this.setState({txtInstructions: ''}); // clear text area
   }
 
-  
+
   /**
    * Toggles ability to submit instructions.
-   * We need at least 3 lines to try and do anything valuable.
+   * We need at least 2 or 3 lines to try and do anything valuable
    */
   isValidInstruction = () => {
+    const compareTo = bounds.isSet ? 2 : 3;
     return !(this.state.txtInstructions.length >= 0 &&
-      this.state.txtInstructions.trim().split('\n').length >= 3);
+      this.state.txtInstructions.trim().split('\n').length >= compareTo);
   }
 
-  
+
   /**
    * Does what it says on the label: clears text from instruction area
    */
@@ -107,9 +109,9 @@ export default class Instruct extends Component {
           </textarea>
         </label>
         <div className='small expanded button-group'>
-          <button title='Run Instructions' className='success button' disabled={this.isValidInstruction()} >Instruct</button>
+          <button title='Run Instructions' className='success button' type='submit' disabled={this.isValidInstruction()} >Instruct</button>
           <button title='Insert Demo Instructions' className='button' type='button' onClick={this.setDemoText} >Demo</button>
-          <button title='Clear Instructions' className='button' disabled={this.state.txtInstructions.length === 0} type='button' onClick={this.resetForm} >Clear</button>
+          <button title='Clear Instructions' className='button' type='button' disabled={this.state.txtInstructions.length === 0} onClick={this.resetForm} >Clear</button>
         </div>
       </form>
     );
